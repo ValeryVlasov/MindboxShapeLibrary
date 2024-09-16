81998e0eba4e5b6caba29d7eb222b81db7578ccf
Валерий Власов <valera.vlasov11@mail.ru>
2024-09-16 15:15:54 +0300

    Move Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -1,4 +1,4 @@
-﻿SELECT p.name as ProductName, c.name as CategoryName
+SELECT p.name as ProductName, c.name as CategoryName
 FROM Product p
 	LEFT JOIN ProductCategory pc on p.id = pc.product_id
 	LEFT JOIN Category c on pc.category_id = c.id
